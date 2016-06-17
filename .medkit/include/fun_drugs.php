9e330d8b9b4747d43de26b5214102edd0a365159
Piotr <piotrolbrot@gmail.com>
2016-06-17 19:08:22 +0200

    db_statement is taking over

--- .medkit/include/fun_drugs.php
+++ .medkit/include/fun_drugs.php
@@ -7,24 +7,7 @@
         $sql = "INSERT INTO DrugsDB (name, price, overdue, user_added, group_id)
                     VALUES (?,?,?,?,?)";
 
-        $stmt = mysqli_prepare($dbConnection,$sql);
-        if ($stmt === false) {
-            trigger_error('Statement failed! ' . htmlspecialchars(mysqli_error($dbConnection)), E_USER_ERROR);
-        }
-
-        $bind = mysqli_stmt_bind_param($stmt, "sissi", $drugName, $drugPrice, $drugDate, $username, $groupID);
-        if ($bind === false) {
-            trigger_error('Bind param failed!', E_USER_ERROR);
-        }
-
-        $exec = mysqli_stmt_execute($stmt);
-        if ($exec === false) {
-            trigger_error('Statement execute failed! ' . htmlspecialchars(mysqli_stmt_error($stmt)), E_USER_ERROR);
-        }
-
-        mysqli_stmt_close($stmt);
-        mysqli_close($dbConnection);
-
+        $processed = db_statement($sql, "sissi", array(&$drugName, &$drugPrice, &$drugDate, &$username, &$groupID));
     }
 
     function drugs_print_table($groupID){
@@ -33,9 +16,9 @@
 
         $sql = "SELECT id, name, price, amount, overdue, user_added 
                     FROM DrugsDB 
-                    WHERE group_id = $groupID";
+                    WHERE group_id = ?";
 
-        $result = mysqli_query($dbConnection, $sql);
+        $result = db_statement($sql, "i", array(&$groupID));
 
         if (mysqli_num_rows($result) > 0) {
 
@@ -80,71 +63,48 @@
                 "<a href='drugs_new.php'>Dodaj nowy lek</a>";
 
         }
-
-        mysqli_close($dbConnection);
     }
 
     function drugs_delete_record($drugID, $groupID){
 
         require("config/sql_connect.php");
+        
 
         $sql = "DELETE FROM DrugsDB 
                     WHERE id = ?
                     AND group_id = ?";
 
-        $stmt = mysqli_prepare($dbConnection,$sql);
-        if ($stmt === false) {
-            trigger_error('Statement failed! ' . htmlspecialchars(mysqli_error($dbConnection)), E_USER_ERROR);
-        }
-
-        $bind = mysqli_stmt_bind_param($stmt, "ii", $drugID, $groupID);
-        if ($bind === false) {
-            trigger_error('Bind param failed!', E_USER_ERROR);
-        }
-
-        $exec = mysqli_stmt_execute($stmt);
-        if ($exec === false) {
-            trigger_error('Statement execute failed! ' . htmlspecialchars(mysqli_stmt_error($stmt)), E_USER_ERROR);
-        }
-
-        mysqli_stmt_close($stmt);
-        mysqli_close($dbConnection);
-
+        $processed = db_statement($sql, "ii", array(&$drugID, &$groupID));
     }
 
     function drugs_overdue_check_date($groupID){
 
         require("config/sql_connect.php");
+        
 
         $sql = "SELECT id, name, overdue, amount
                     FROM DrugsDB 
-                    WHERE group_id = $groupID
+                    WHERE group_id = ?
                     AND DATE(overdue) < CURRENT_DATE()";
 
-        $result = mysqli_query($dbConnection, $sql);
+        $result = db_statement($sql, "i", array(&$groupID));
 
-        if (mysqli_num_rows($result) > 0){
-
-            return true;
-
-        }
-
-        mysqli_close($dbConnection);
-
-        return false;
+        if (mysqli_num_rows($result) > 0) return true;
+        else return false;
 
     }
 
     function drugs_overdue_print_table($groupID){
 
         require("config/sql_connect.php");
+        
 
         $sql = "SELECT id, name, overdue, amount
                     FROM DrugsDB 
-                    WHERE group_id = $groupID
+                    WHERE group_id = ?
                     AND DATE(overdue) < CURRENT_DATE()";
 
-        $result = mysqli_query($dbConnection, $sql);
+        $result = db_statement($sql, "i", array(&$groupID));
 
         if (mysqli_num_rows($result) > 0) {
 
@@ -183,9 +143,6 @@
             "<p>Wszystkie leki znajdujące się w apteczce są przydatne do spożycia.</p>";
 
         }
-
-        mysqli_close($dbConnection);
-        
     }
 
     function drugs_overdue_soon_print_table($groupID, $soonInt){
@@ -194,11 +151,11 @@
 
         $sql = "SELECT id, name, overdue, amount
                     FROM DrugsDB 
-                    WHERE group_id = $groupID
-                    AND DATE(overdue) < CURRENT_DATE() + INTERVAL $soonInt day
+                    WHERE group_id = ?
+                    AND DATE(overdue) < CURRENT_DATE() + INTERVAL ? day
                     AND DATE(overdue) > CURRENT_DATE()";
 
-        $result = mysqli_query($dbConnection, $sql);
+        $result = db_statement($sql, "ii", array(&$groupID, &$soonInt));
 
         if (mysqli_num_rows($result) > 0) {
 
@@ -247,9 +204,6 @@
             "<p>Okres ważności wszystkich pozostałych leków w apteczce jest dłuższy niż $soonInt dni.</p>";
 
         }
-
-        mysqli_close($dbConnection);
-
     }
 
 ?>
